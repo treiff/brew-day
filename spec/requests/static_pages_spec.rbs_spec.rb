7e8dc51091e6304fe6c6c344ab5dbac9321a6113
treiff <trevorreiff@gmail.com>
2014-08-04 21:52:28 -0400

    modify static pages spec and content to simplify.

--- spec/requests/static_pages_spec.rbs_spec.rb
+++ spec/requests/static_pages_spec.rbs_spec.rb
@@ -8,33 +8,48 @@ describe "Static Pages" do
       expect(page).to have_content('Who Brew')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/pages/home'
-      expect(page).to have_title("Who Brew | Home")
+      expect(page).to have_title("Who Brew")
+    end
+
+    it "should not have custom page title" do
+      visit '/pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
   describe "About Page" do
-    it "should have content 'About'" do
+    it "should have content 'Who Brew'" do
       visit '/pages/about'
-      expect(page).to have_content('About')
+      expect(page).to have_content('Who Brew')
     end
 
-    it "should have the title 'About'" do
+    it "should have the base title" do
       visit '/pages/about'
-      expect(page).to have_title("Who Brew | About")
+      expect(page).to have_title('Who Brew')
+    end
+
+    it "should not have custom page title" do
+      visit '/pages/about'
+      expect(page).not_to have_title('| About')
     end
   end
 
   describe "Contact Page" do 
     it "should have content 'Contact'" do
       visit '/pages/contact'
-      expect(page).to have_content('Contact')
+      expect(page).to have_content('Who Brew')
     end
 
-    it "should have the title 'Contact'" do
+    it "should have the base title" do
       visit '/pages/contact'
-      expect(page).to have_title("Who Brew | Contact")
+      expect(page).to have_title('Who Brew')
+    end
+
+    it "should not have custom page title" do
+      visit '/pages/contact'
+      expect(page).not_to have_title('| Contact')
     end
   end
 end
