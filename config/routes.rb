2adbe2a8b13ebf53aa8c5f4998b3237492043e9a
treiff <trevorreiff@gmail.com>
2014-08-06 22:23:00 -0400

    reconfigured routes, added containers for about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  root 'pages#home'
+  match 'help',     to: 'pages#help',      via: 'get'
+  match 'about',    to: 'pages#about',     via: 'get'
+  match 'contact',  to: 'pages#contact',   via: 'get'
 end
--- spec/requests/static_pages_spec.rbs_spec.rb
+++ spec/requests/static_pages_spec.rbs_spec.rb
@@ -1,55 +1,28 @@
 require 'rails_helper'
 
 describe "Static Pages" do
+
+  subject { page }
   
   describe "Home Page" do 
-    it "should have the content 'Who Brew'" do
-      visit '/pages/home'
-      expect(page).to have_content('Who Brew')
-    end
+    before { visit root_path }
 
-    it "should have the base title" do
-      visit '/pages/home'
-      expect(page).to have_title("Who Brew")
-    end
-
-    it "should not have custom page title" do
-      visit '/pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Who Brew') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "About Page" do
-    it "should have content 'Who Brew'" do
-      visit '/pages/about'
-      expect(page).to have_content('Who Brew')
-    end
+    before { visit about_path }
 
-    it "should have the base title" do
-      visit '/pages/about'
-      expect(page).to have_title('Who Brew')
-    end
-
-    it "should not have custom page title" do
-      visit '/pages/about'
-      expect(page).not_to have_title('| About')
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
   end
 
-  describe "Contact Page" do 
-    it "should have content 'Contact'" do
-      visit '/pages/contact'
-      expect(page).to have_content('Who Brew')
-    end
+  describe "Contact Page" do
+    before { visit contact_path }
 
-    it "should have the base title" do
-      visit '/pages/contact'
-      expect(page).to have_title('Who Brew')
-    end
-
-    it "should not have custom page title" do
-      visit '/pages/contact'
-      expect(page).not_to have_title('| Contact')
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
