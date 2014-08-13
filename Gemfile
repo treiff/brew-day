d11b1a34cf9ee26e718311311cddb29641772cb6
treiff <trevorreiff@gmail.com>
2014-08-12 21:30:18 -0400

    added gmaps gem. started configuring

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'devise'
+gem 'gmaps4rails'
 
 group :doc do 
   gem 'sdoc', '~> 0.4.0', require: false
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,11 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require gmaps/google
+//= require underscore
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
