d45690de1edd8ecf80b1422deb5236786216dd1e
Trevor Reiff <trevorreiff@gmail.com>
2015-01-05 20:46:37 -0500

    add date validation to brewsession model

--- app/models/brew_session.rb
+++ app/models/brew_session.rb
@@ -1,3 +1,6 @@
 class BrewSession < ActiveRecord::Base
   belongs_to :user
+
+  validates_datetime :brewing_date, :on_or_after => lambda { Date.current }, :before => :create,
+                                    :on_or_after_message => 'Date must be in future'
 end
